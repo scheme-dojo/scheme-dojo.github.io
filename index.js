b5ec1dbc88369a6c47a1a6caf71bf19df4cc2e8a
rabiarz <rabiarezaie21@gmail.com>
2024-06-27 17:33:56 +0900

    final pages

---
+++ index.js
@@ -0,0 +1,16 @@
+const correctAnswer = "(banana orange)"
+
+const checkAnswer = () => {
+
+  const userAnswer = document.getElementById('input-tag').value;
+
+  console.log(userAnswer);
+  if (userAnswer === correctAnswer) {
+    window.location.href ="welcome.html"
+  } else {
+    window.location.href ="hint.html"
+  }
+}
+
+document.getElementById('enter-btn').addEventListener('click', checkAnswer);
+
